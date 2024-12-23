4024133ad4ec82ecf1a2877bdfba74a678f7f6ce
Felix Brüll <felixbruell@live.de>
2024-12-23 21:03:07 +0100

    alice ch7

--- voice/deploy_audio.py
+++ voice/deploy_audio.py
@@ -2,9 +2,11 @@ import os
 from pydub import AudioSegment
 
 # Define source and destination directories, and overwrite flag
-SOURCE_DIR = "C:/git/AliceResources/voice"
-DEST_DIR = "C:/git/AliceWonderland/game/voice"
-OVERWRITE = True  # Set to False to skip existing files
+#SOURCE_DIR = "C:/git/AliceResources/voice"
+#DEST_DIR = "C:/git/AliceWonderland/game/voice"
+SOURCE_DIR = "C:/Users/Felix/Documents/git/alice/AliceResources/voice"
+DEST_DIR = "C:/Users/Felix/Documents/git/alice/AliceInWonderland/game/voice"
+OVERWRITE = False  # Set to False to skip existing files
 SAMPLE_RATE = 44100  # Configurable sample rate (44100 by default)
 FORCE_MONO = True  # Force mono audio for mp3 export
 
@@ -24,6 +26,8 @@ CHARACTER_SETTINGS = {
     "caterpillar": {"gain": 9},
     "mock": {"gain": -4},
     "frogfoot": {"gain": 7},
+    "hatter": {"gain": 2},
+    "dormouse": {"gain": 4},
 }
 
 def get_character_settings(file_name):
